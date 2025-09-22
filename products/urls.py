1e05bb84c9600ee0e1f62d2afbd433932db17baa
Rafay <rr0452614@gmail.com>
2025-09-22 15:43:50 +0500

    n

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 
-urlpatterns=[
-
-    path('', views.index)
+urlpatterns=[                     #ignore errors here
+    path('', views.index),
+    path('new', views.new)
 ]
--- products/views.py
+++ products/views.py
@@ -2,4 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index (request):
-    return HttpResponse("My site")
+    return HttpResponse("Hello World")
+
+def new (request):
+    return HttpResponse("New products")
