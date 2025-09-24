3ac2f81bae22d97f91ee37d6717b9139f09d18c8
Rafay <rr0452614@gmail.com>
2025-09-24 15:58:48 +0500

    n

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import Products
+from .models import Product, Offer
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+
+admin.site.register(Product, ProductAdmin)
 
 
