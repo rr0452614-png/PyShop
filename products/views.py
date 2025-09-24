84d81169e46948066e58fed5160a58efca3a76a4
Rafay <rr0452614@gmail.com>
2025-09-24 16:14:50 +0500

    v

--- products/views.py
+++ products/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 def index (request):
+    products = Product.objects.all()
+    
     return HttpResponse("Hello World")
 
 def new (request):
